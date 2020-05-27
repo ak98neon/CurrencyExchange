f2a41c0fe72253b04d67994b5ad58919a410472d
ak98neon <ak98nneon@gmail.com>
2020-05-27 16:30:35 +0300

    Fix afterMigrate.sql

--- src/main/resources/db/migration/testdata/afterMigrate.sql
+++ src/main/resources/db/migration/testdata/afterMigrate.sql
@@ -1,27 +1,18 @@
-TRUNCATE users RESTART IDENTITY CASCADE;
-TRUNCATE role_users CASCADE;
-TRUNCATE role CASCADE;
-TRUNCATE commissions CASCADE;
-TRUNCATE exchange_rates CASCADE;
+truncate users RESTART IDENTITY CASCADE;
+truncate commissions CASCADE;
+truncate exchange_rates CASCADE;
 
-INSERT INTO role (name)
-VALUES ('ROLE_ADMIN'), ('ROLE_USER');
-
-INSERT INTO users (username, password, email)
-VALUES ('admin', '$2a$10$4AlJB9RY1DRHyxD4qm.VSe6plBvxH/zh/XysnQHhzHbWNz0Ai9SqG', 'admin@gmail.com'),
+insert into users (username, password, email)
+values ('admin', '$2a$10$4AlJB9RY1DRHyxD4qm.VSe6plBvxH/zh/XysnQHhzHbWNz0Ai9SqG', 'admin@gmail.com'),
        ('user', '$2a$10$4AlJB9RY1DRHyxD4qm.VSe6plBvxH/zh/XysnQHhzHbWNz0Ai9SqG', 'user@gmail.com'),
        ('ivan', '$2a$10$4AlJB9RY1DRHyxD4qm.VSe6plBvxH/zh/XysnQHhzHbWNz0Ai9SqG', 'ivan@gmail.com');
 
-INSERT INTO role_users (role_id, user_id)
-        (SELECT id, (SELECT id FROM users WHERE username = 'admin') FROM role WHERE name = 'ROLE_ADMIN');
-INSERT INTO role_users (role_id, user_id)
-        (SELECT id, (SELECT id FROM users WHERE username = 'admin') FROM role WHERE name = 'ROLE_USER');
+insert into commissions (from_currency, to_currency, value) values ('EUR', 'UAH', 5);
+insert into commissions (from_currency, to_currency, value) values ('UAH', 'EUR', 5);
+insert into commissions (from_currency, to_currency, value) values ('USD', 'UAH', 3);
+insert into commissions (from_currency, to_currency, value) values ('UAH', 'USD', 3);
 
-INSERT INTO commissions (id, from_currency, to_currency, value) VALUES (1, 'EUR', 'UAH', 27);
-INSERT INTO commissions (id, from_currency, to_currency, value) VALUES (2, 'USD', 'UAH', 3);
-INSERT INTO commissions (id, from_currency, to_currency, value) VALUES (3, 'UAH', 'USD', 3);
-
-INSERT INTO exchange_rates (id, from_currency, to_currency, rate) VALUES (1, 'USD', 'UAH', 27);
-INSERT INTO exchange_rates (id, from_currency, to_currency, rate) VALUES (3, 'USD', 'RUB', 70);
-INSERT INTO exchange_rates (id, from_currency, to_currency, rate) VALUES (4, 'RUB', 'USD', 0.014);
-INSERT INTO exchange_rates (id, from_currency, to_currency, rate) VALUES (5, 'UAH', 'USD', 0.037);
+insert into exchange_rates (from_currency, to_currency, rate) values ('USD', 'UAH', 27);
+insert into exchange_rates (from_currency, to_currency, rate) values ('USD', 'RUB', 70);
+insert into exchange_rates (from_currency, to_currency, rate) values ('RUB', 'USD', 0.014);
+insert into exchange_rates (from_currency, to_currency, rate) values ('UAH', 'USD', 0.037);
